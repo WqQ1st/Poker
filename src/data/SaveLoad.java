60fa14ac6ea0192261f2058677a7f84399dcb849
Shane <shane_matsumura@berkeley.edu>
2026-01-14 19:02:41 -0800

    restructured project, is now maven

--- src/data/SaveLoad.java
+++ src/data/SaveLoad.java
@@ -1,5 +1,25 @@
 package data;
 
-public class SaveLoad {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class SaveLoad {
+    private final ObjectMapper mapper;
+
+    public SaveLoad() {
+        mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+    }
+
+    public void save(Path path, GameState state) throws Exception {
+        Files.createDirectories(path.getParent());
+        mapper.writeValue(path.toFile(), state);
+    }
+
+    public GameState load(Path path) throws Exception {
+        return mapper.readValue(path.toFile(), GameState.class);
+    }
 }
